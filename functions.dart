1ea266ba99b3fe09346fe0e001827377c2d6c88f
jaeyoon lee <rainbowcrayon91@gmail.com>
2023-03-05 22:28:40 +0900

    functions(2)

--- functions.dart
+++ functions.dart
@@ -30,6 +30,34 @@ String sayWhoIam3({
   return "hello i'm $name, i'm $age and i'm from $country";
 }
 
+// 3.3 optional positional parameter
+String sayBye(String name, int age, [String? country = 'cuba']) =>
+    "hello, I'm $name, I'm $age, and I'm from $country";
+
+// 3.4 QQ Operator
+String capitalizeName(String? name) {
+  if (name != null) {
+    return name.toUpperCase();
+  }
+  return 'ANNO';
+}
+
+String capitalizeNmae2(String? name) =>
+    name != null ? name.toUpperCase() : 'ANNO';
+String capitalizeNmae3(String? name) => name?.toUpperCase() ?? 'ANNO';
+
+// 3.5 typedef
+typedef ListOfInt = List<int>;
+ListOfInt reverseListOfNumbers(ListOfInt list) {
+  var reverseList = list.reversed;
+  return reverseList.toList();
+}
+
+typedef UserInfo = Map<String, String>;
+String sayHi(UserInfo) {
+  return "Hi, ${UserInfo['name']}";
+}
+
 void main() {
   print(sayHello('jaeyoon'));
   print(sayGoodbye('nico'));
@@ -50,4 +78,18 @@ void main() {
     age: 30,
     country: 'use',
   ));
+
+  print(sayBye(
+    'nico',
+    14,
+  ));
+  // 3.4 QQ Operation
+  // subject 가 null이면 math를 할당해라.
+  String? subject;
+  subject ??= 'math';
+  subject ??= 'english';
+  print(subject);
+
+  // 3.5 typedef
+  print(sayHi({"name": "jaeyon"}));
 }
