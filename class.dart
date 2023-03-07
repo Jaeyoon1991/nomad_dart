48dd53bc6706339ce305da11dd00fb38c063e3ea
jaeyoon lee <rainbowcrayon91@gmail.com>
2023-03-07 22:37:06 +0900

    classes(2) : 4.4 Recap

--- class.dart
+++ class.dart
@@ -39,17 +39,161 @@ class Player3 {
   }
 }
 
+// 4.2 Named Constructor Parameters
+class Player4 {
+  late String name;
+  late int xp;
+  late String team;
+  late int age;
+
+  Player4(
+    this.name,
+    this.xp,
+    this.team,
+    this.age,
+  );
+
+  void sayHello() {
+    print(
+        "hello, my name is $name, and $xp, my tema is $team, and i'm $age years");
+  }
+}
+
+class Player5 {
+  final String name;
+  int xp;
+  String team;
+  int age;
+
+  Player5(
+      {required this.name,
+      required this.xp,
+      required this.team,
+      required this.age});
+
+  void sayHello() {
+    print(
+        "hello, my name is $name, and $xp, my tema is $team, and i'm $age years");
+  }
+}
+
+// 4.3 Named Constructor
+class Player6 {
+  final String name;
+  int xp, age;
+  String team;
+
+  Player6(
+      {required this.name,
+      required this.xp,
+      required this.team,
+      required this.age});
+
+  Player6.createBluePlayer({
+    required String name,
+    required int age,
+  })  : this.name = name,
+        this.age = age,
+        this.team = 'blue',
+        this.xp = 0;
+
+  Player6.createRedPlayer(
+    String name,
+    int age,
+  )   : this.name = name,
+        this.age = age,
+        this.team = 'red',
+        this.xp = 0;
+
+  void sayHello() {
+    print(
+        "hello, my name is $name, and $xp, my tema is $team, and i'm $age years");
+  }
+}
+
+// 4.4 Recap
+class Player7 {
+  final String name;
+  int xp;
+  String team;
+
+  Player7.fromJson(Map<String, dynamic> playerJson)
+      : name = playerJson['name'],
+        xp = playerJson['xp'],
+        team = playerJson['team'];
+
+  void sayHello() {
+    print("hello, my name is $name, and my xp is $xp, and my team is $team");
+  }
+}
+
 void main() {
   var player = Player();
   print(player.name);
   player.name = 'lalal';
   print(player.name);
+
   // student는 final이기 때문에 name 처럼 수정이 불가능하다.
   player.sayHello();
 
-  var player2 = Player2('emma', 2000);
+  var player2 = Player2(
+    'emma',
+    2000,
+  );
   player2.sayHello();
 
-  var player3 = Player3('harry', 4000);
+  var player3 = Player3(
+    'harry',
+    4000,
+  );
   player3.sayHello();
+
+  var player4 = Player4(
+    'jas',
+    3000,
+    'red',
+    39,
+  );
+  player4.sayHello();
+
+  var player5 = Player5(
+    name: "nico",
+    xp: 3000,
+    team: 'blue',
+    age: 30,
+  );
+  player5.sayHello();
+
+  var BluePlayer = Player6.createBluePlayer(
+    name: 'haa',
+    age: 34,
+  );
+
+  var RedPlayer = Player6.createRedPlayer(
+    'dodo',
+    33,
+  );
+
+  var apiData = [
+    {
+      'name': 'nico',
+      'team': 'red',
+      'xp': 1000,
+    },
+    {
+      'name': 'anna',
+      'team': 'blue',
+      'xp': 3000,
+    },
+    {
+      'name': 'coco',
+      'team': 'green',
+      'xp': 5000,
+    }
+  ];
+
+  apiData.forEach((playerJson) {
+    var player = Player7.fromJson(playerJson);
+    player.sayHello();
+  });
 }
