43c5c6670a9231cd8a02af280f3fa25f6f2a3642
jaeyoon lee <rainbowcrayon91@gmail.com>
2023-03-10 21:40:53 +0900

    classes(5) : 4.9 Mixins&4.10 Conclusions

--- class_inheritance.dart
+++ class_inheritance.dart
@@ -1,3 +1,34 @@
+// 4.9 Mixins
+// mixin은 단순히 내부 프로퍼티를 이용할 수 있게만 할 뿐. 상속이랑 다르다.
+// 생성자가 없는 클래스여야 한다.
+class Strong {
+  final double strongLevel = 1500.99;
+}
+
+class QuickRunner {
+  void runQuick() {
+    print("runnnn");
+  }
+}
+
+class Tall {
+  double heightLevel = 190.99;
+}
+
+class Player2 with Strong, QuickRunner, Tall {}
+
+class Horse with QuickRunner, Tall {}
+
+class Boy with Tall, Strong {
+  final Team team;
+  final String name;
+
+  Boy({
+    required this.team,
+    required this.name,
+  });
+}
+
 class Human {
   final String name;
   Human({
@@ -31,4 +62,7 @@ void main() {
     name: 'nico',
   );
   player.sayHello();
+
+  var boy = Boy(team: Team.red, name: 'nico');
+  boy.heightLevel = 19.22;
 }
